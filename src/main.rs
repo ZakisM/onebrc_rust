7895d473056d97864c774a16515d2aea46e9aa99
zak <8143258+ZakisM@users.noreply.github.com>
2024-04-13 22:34:50 +0100

    Read in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use ahash::AHashMap;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use std::{
     fs::File,
-    io::{BufReader, Read},
+    io::{Read, Seek, SeekFrom},
+    os::unix::fs::FileExt,
     thread,
 };
 
 use memchr::memchr2_iter;
 
-fn process_chunk(chunk: Vec<u8>) {
-    // let mut res = AHashMap::with_capacity(448);
+fn process_chunk(start: usize, end: usize) {
+    let file = File::open("../../IdeaProjects/1brc_typescript/measurements.txt").unwrap();
+    let mut chunk = vec![0; end - start];
+    file.read_exact_at(&mut chunk, u64::try_from(start).unwrap())
+        .unwrap();
 
     let mut it = memchr2_iter(b';', b'\n', &chunk);
     let mut offset = 0;
@@ -22,56 +27,54 @@ fn process_chunk(chunk: Vec<u8>) {
         let city = &chunk[offset..semi_colon];
         let temp = &chunk[semi_colon + 1..nl];
 
-        // res.insert(city, temp);
-
         offset = nl + 1;
     }
 }
 
 fn main() -> eyre::Result<()> {
-    let start = std::time::Instant::now();
+    let start_time = std::time::Instant::now();
 
-    let file = File::open("../../IdeaProjects/1brc_typescript/measurements.txt")?;
-    let file_size = file.metadata()?.len();
+    let mut file = File::open("../../IdeaProjects/1brc_typescript/measurements.txt")?;
+    let file_size: usize = (file.metadata()?.len()).try_into()?;
     let num_cpus = thread::available_parallelism()?.get();
-    let chunk_size = file_size as usize / num_cpus;
+    let chunk_size = file_size / num_cpus;
 
-    let mut reader = BufReader::with_capacity(chunk_size, file);
+    let mut chunk_indexes = Vec::with_capacity(num_cpus);
+    let mut start = 0_usize;
 
-    let mut line = Vec::with_capacity(chunk_size + 31);
-
-    let pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(num_cpus)
-        .build()?;
-    // let mut handles = Vec::with_capacity(12);
-
-    while let Ok(read) = reader
-        .by_ref()
-        .take(chunk_size as u64)
-        .read_to_end(&mut line)
-    {
-        if read == 0 {
+    let mut curr_chunk = Vec::with_capacity(100);
+    loop {
+        if start + chunk_size > file_size {
+            chunk_indexes.push((start, file_size));
             break;
         }
 
-        let nl = line
+        file.seek(SeekFrom::Current(i64::try_from(chunk_size)?))?;
+        // TODO:
+        // file.read_exact_at(, )
+
+        file.by_ref().take(100).read_to_end(&mut curr_chunk)?;
+
+        let nl = curr_chunk
             .iter()
             .rposition(|&x| x == 10)
             .expect("Line ending missing in chunk");
 
-        // Safety: Should re-check this if we change CHUNK_SIZE
-        // let line_string = unsafe { String::from_utf8_unchecked(line.clone()) };
+        let nl = nl + usize::try_from(file.stream_position()?)?;
 
-        let line_thread = line.clone();
-        pool.spawn(|| {
-            process_chunk(line_thread);
-        });
+        chunk_indexes.push((start, nl));
 
-        line.copy_within(nl + 1.., 0);
-        line.truncate(line.len() - nl - 1);
+        start = nl + 1;
+        curr_chunk.clear();
+        file.seek(SeekFrom::Start(u64::try_from(start)?))?;
     }
 
-    println!("That took: {}ms", &start.elapsed().as_millis());
+    let res = chunk_indexes
+        .into_par_iter()
+        .map(|(start, end)| process_chunk(start, end))
+        .collect::<Vec<_>>();
+
+    println!("That took: {}ms", &start_time.elapsed().as_millis());
 
     Ok(())
 }
